48fa99a8757f2000379e55eedee5938a31c593fd
Issa sangare <issabmsangare@hotmail.com>
2020-12-10 12:02:31 -0500

    complete layout, homepage, work, about

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express")
  
 const app = express();
 let items = ["Buy food", "Cook food", "Eat food"] 
+let workItems = []
 app.use(express.static("public")); 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
@@ -18,19 +19,38 @@ app.get("/", (req, res) =>{
     }
 
     let day = today.toLocaleDateString("en-us", options)
-    res.render('list', {kindOfDay: day, newListItems: items });
-})
+    res.render('list', {listTitle: day, newListItems: items });
+});
 
 app.post("/", (req, res) =>{
+    console.log(req.body)
     item = req.body.newItem;
+    if(req.body.list === "work"){
+        workItems.push(item);
+        res.redirect("/work")
+    }else {
+        items.push(item)
+        res.redirect("/")
+    }
     
-    items.push(item)
-    console.log(item)
+    
+});
 
-    res.redirect("/")
-
+app.get("/work", (req, res) =>{
+    res.render("list", {listTitle: "work List", newListItems: workItems})
+    
 })
  
+app.post("/work", (req, res) =>{
+    let item = req.body.newItem;
+    console.log(item)
+    workItems.push(item);
+    res.redirect("/work")
+});
+
+app.get("/about", (req, res) =>{
+    res.render("about")
+})
  
 app.listen(3000, () =>{
    console.log("Server is running")
