b76921ead746025ae9a225b804cdcfdc855b0ba0
Issa sangare <issabmsangare@hotmail.com>
2020-12-10 03:19:43 -0500

    complete form and post request

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const bodyParser = require("body-parser")
 const express = require("express")
 
  
-const app = express()
+const app = express();
+let items = ["Buy food", "Cook food", "Eat food"] 
 app.use(express.static("public")); 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
@@ -10,41 +11,25 @@ app.set('view engine', 'ejs');
 app.get("/", (req, res) =>{
     let today = new Date();
     // console.log(today.getHours())
-    let currentDay = today.getDay()
-    let day = "";
-    switch (currentDay) {
-        case 0:
-            day = "Sunday"
-            break;
-        case 1:
-            day = "Monday"
-            break;
-        case 2:
-            day = "Tuesday"
-            break;
-        case 3:
-            day = "Wednesday"
-            break;
-        case 4:
-            day = "Thursday"
-            break;
-        case 5:
-            day = "Friday"
-            break;
-        case 6:
-            day = "Saturday"
-            break;
-    
-        default:
-            console.log("Error: the day is" + currentDay )
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
     }
-    res.render('list', {kindOfDay: day});
-})
-// app.set('view engine', 'ejs');
 
-// app.get('/', (req, res) => {
-//   res.render('index', {foo: 'FOO'});
-// });
+    let day = today.toLocaleDateString("en-us", options)
+    res.render('list', {kindOfDay: day, newListItems: items });
+})
+
+app.post("/", (req, res) =>{
+    item = req.body.newItem;
+    
+    items.push(item)
+    console.log(item)
+
+    res.redirect("/")
+
+})
  
  
 app.listen(3000, () =>{
